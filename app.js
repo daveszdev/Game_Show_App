298df4c855a627b70f9b5265c7f2929966a55009
David Mate <davesz.dev@gmail.com>
2021-02-26 14:50:44 +0100

    firstLines
    I added the variables and the phrases.

---
+++ app.js
@@ -0,0 +1,15 @@
+// I added the needed variables.
+
+const qwerty = document.getElementById('qwerty');
+const phrase = document.getElementById('phrase');
+const buttonReset = document.querySelector('.btn__reset');
+let missed = 0;
+
+//  I added 5 phrases.
+const phrases = [
+  'seashells by the seashore',
+  'eddie edited it',
+  'red lorry yellow lorry',
+  'she sees cheese',
+  'we all scream for ice cream'
+];
